51b4aaf2d9d42e2a977bcab6542dc842cf64d428
LuisRGN <luisrafael1598@gmail.com>
2024-04-02 15:44:46 -0500

    se hace la ruta y el crud de los turnos

---
+++ back/src/controllers/indexControllers/turnsController.ts
@@ -0,0 +1,6 @@
+import { getTurns, getTurnId } from "../Turns/getTurn";
+import { deleteTurns } from "../Turns/deleteTurn";
+import { putTurns } from "../Turns/putTurn";
+import { postTurns } from "../Turns/postTurn";
+
+export { getTurns, getTurnId, deleteTurns, putTurns, postTurns };
