68e041ab06fa62a64c0b6d5950cdb84a6a9d6495
LuisRGN <luisrafael1598@gmail.com>
2024-04-02 21:22:17 -0500

    se configura la carpeta config para la conexion a la BD

---
+++ back/src/config/repository.ts
@@ -0,0 +1,5 @@
+import { AppDataSource } from "./data_source";
+
+export const userModel = AppDataSource.getRepository
+export const turnModel = AppDataSource.getRepository
+export const credentialModel = AppDataSource.getRepository
