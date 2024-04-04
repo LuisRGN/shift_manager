71af2e1f3ea871a8ec99ec84b5cf4ca6bbe87227
LuisRGN <luisrafael1598@gmail.com>
2024-04-04 13:08:20 -0500

    correcion del delete

--- back/src/services/Users/deleteUserServices.ts
+++ back/src/services/Users/deleteUserServices.ts
@@ -1,14 +1,15 @@
-import { userModel } from "../../config/repository"
-import { User } from "../../entities/User"
-
-/* ↓ Funcion que elimina al usuario ↓ */
+import { credentialModel, userModel } from "../../config/repository";
+import { User } from "../../entities/User";
 
 export const deleteUserServices = async (userId: number): Promise<void> => {
-    const deleteUser: User | null = await userModel.findOneBy({ id: userId })
-    if (deleteUser) {
-        await userModel.remove(deleteUser)
-        console.log("Usuario eliminado correctamente")
-    } else {
-        throw Error("No se encontro el usuario")
+    const deleteUser: User | null = await userModel.findOneBy({ id: userId });
+    if (!deleteUser) {
+        throw Error("No se encontro el usuario");
     }
-}
+
+    await credentialModel.delete({ user: deleteUser })
+
+    await userModel.remove(deleteUser);
+
+    console.log("Usuario eliminado correctamente");
+};
