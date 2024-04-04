36ebe4a9a70e4b1311c536902e40a06dd46be2f2
LuisRGN <luisrafael1598@gmail.com>
2024-04-04 12:02:02 -0500

    se conectan los servicios a los controladores

--- back/src/controllers/users/postUser.ts
+++ back/src/controllers/users/postUser.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express"
+import { postLoginServices, postReisterServices } from "../../services/indexServices/userServices"
+import { validateCredential } from "../../services/Credentials/createCredentialsServices"
 
 export const postRegisterUser = async (req: Request, res: Response): Promise<void> => {
+    const { name, email, birthdate, dni, username, password } = req.body
     try {
-        res.status(200).json({ message: "post register user " })
+        const newUser = await postReisterServices({ name, email, birthdate, dni, username, password })
+        res.status(200).json(newUser)
     } catch (error) {
         res.status(400).json({ message: error })
     }
 }
 
 export const postLoginUser = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
     try {
-        res.status(200).json({ message: "post login user" })
+        const userLogin = await validateCredential({ username, password })
+        const user = await postLoginServices(userLogin.id)
+        res.status(200).json({ login: true, user })
     } catch (error) {
         res.status(400).json({ message: error })
     }
