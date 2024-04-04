12fa4071255be4deba7e21cf3f0f18479e5f3b6f
LuisRGN <luisrafael1598@gmail.com>
2024-04-04 12:18:16 -0500

    se conectan los servivos a los controladores

--- back/src/controllers/Turns/getTurn.ts
+++ back/src/controllers/Turns/getTurn.ts
@@ -1,16 +1,20 @@
 import { Request, Response } from "express"
+import { getTurnIdServices, getTurnServices } from "../../services/indexServices/turnService"
 
 export const getTurns = async (req: Request, res: Response): Promise<void> => {
     try {
-        res.status(200).json({ message: "get turns" })
+        const tursAll = await getTurnServices()
+        res.status(200).json(tursAll)
     } catch (error) {
         res.status(400).json({ message: error })
     }
 }
 
-export const getTurnId = async (req: Request, res: Response) => {
+export const getTurnId = async (req: Request, res: Response): Promise<void> => {
+    const { id } = req.params
     try {
-        res.status(200).json({ message: "get turn id" })
+        const turnId = await getTurnIdServices(Number(id))
+        res.status(200).json(turnId)
     } catch (error) {
         res.status(400).json({ message: error })
     }
--- back/src/controllers/Turns/postTurn.ts
+++ back/src/controllers/Turns/postTurn.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express"
+import { postTurnServices } from "../../services/Turns/postTurnServices"
 
 export const postTurns = async (req: Request, res: Response): Promise<void> => {
+    const { date, time, userId } = req.body
     try {
-        res.status(200).json({ message: "post turns" })
+        const newTurn = await postTurnServices({ date, time, userId })
+        res.status(200).json(newTurn)
     } catch (error) {
         res.status(400).json({ message: error })
     }
