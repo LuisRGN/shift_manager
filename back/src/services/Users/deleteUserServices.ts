7dc758665ac52e7c6d2d84ec4c0e4f18f86a6ed5
LuisRGN <luisrafael1598@gmail.com>
2024-04-04 11:31:26 -0500

    se marca sus tipos

--- back/src/services/Users/deleteUserServices.ts
+++ back/src/services/Users/deleteUserServices.ts
@@ -1,9 +1,10 @@
 import { userModel } from "../../config/repository"
+import { User } from "../../entities/User"
 
 /* ↓ Funcion que elimina al usuario ↓ */
 
-export const deleteUserServices = async (userId: number) => {
-    const deleteUser = await userModel.findOneBy({ id: userId })
+export const deleteUserServices = async (userId: number): Promise<void> => {
+    const deleteUser: User | null = await userModel.findOneBy({ id: userId })
     if (deleteUser) {
         await userModel.remove(deleteUser)
         console.log("Usuario eliminado correctamente")
