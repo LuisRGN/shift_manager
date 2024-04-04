7a52bd5a6cb9b9cab1d16723dd96ea0698b7cabe
LuisRGN <luisrafael1598@gmail.com>
2024-04-04 11:34:44 -0500

    se importan y se exportan los turnos

---
+++ back/src/services/indexServices/turnService.ts
@@ -0,0 +1,6 @@
+import { getTurnServices, getTurnIdServices } from "../Turns/getTurnServices";
+import { postTurnServices } from "../Turns/postTurnServices";
+import { putTurnSevices } from "../Turns/putTurnServices";
+import { deleteTurnServices } from "../Turns/deleteTurnServices";
+
+export { getTurnServices, getTurnIdServices, postTurnServices, putTurnSevices, deleteTurnServices }
