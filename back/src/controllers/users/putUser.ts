c4e82873fe7d962729e7cdc2fda345509084abf9
LuisRGN <luisrafael1598@gmail.com>
2024-04-06 12:01:01 -0500

    se implementa el put para todos los datos del usuario

--- back/src/controllers/users/putUser.ts
+++ back/src/controllers/users/putUser.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express"
+import { putChangeServices } from "../../services/indexServices/userServices"
 
 export const putUser = async (req: Request, res: Response): Promise<void> => {
+    const { id } = req.params
+    const userData = req.body
+    const credentialData = req.body
     try {
-        res.status(200).json({ message: "put user " })
+        await putChangeServices(Number(id), userData, credentialData)
+        res.status(200).json({ message: "Datos actualzados correctamente" })
     } catch (error) {
-        res.status(400).json({ message: error })
+        res.status(400).json({ message: "No se pudo actualizar el usuario" })
     }
 }
--- back/src/services/indexServices/userServices.ts
+++ back/src/services/indexServices/userServices.ts
@@ -1,5 +1,6 @@
 import { getUserServices, getUserIdServices } from "../Users/getUserServices";
 import { postReisterServices, postLoginServices } from "../Users/postUserServices";
 import { deleteUserServices } from "../Users/deleteUserServices";
+import { putChangeServices } from "../Users/putUserServices"
 
-export { getUserServices, getUserIdServices, postReisterServices, postLoginServices, deleteUserServices };
+export { getUserServices, getUserIdServices, postReisterServices, postLoginServices, deleteUserServices, putChangeServices };
