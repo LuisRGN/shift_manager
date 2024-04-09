6a4a12a06081156a78fb3ffb398268167fd71d3b
LuisRGN <luisrafael1598@gmail.com>
2024-04-08 22:03:34 -0500

    se agrega la funcion para enviar un correo de registro

--- back/src/controllers/users/postUser.ts
+++ back/src/controllers/users/postUser.ts
@@ -3,13 +3,15 @@ import { postLoginServices, postReisterServices } from "../../services/indexServ
 import { validateCredential } from "../../services/Credentials/createCredentialsServices";
 import { User } from "../../entities/User";
 import { Credential } from "../../entities/Credential";
+import { enviarCorreoDeConfirmacion } from "../../services/nodemailer";
 
-/* ↓ Funcion del controlador que registra a un usuario ↓ */
+/* ↓ Funcion del controlador que registra a un usuario, se agrega enviar un correo de confirmación↓ */
 
 export const postRegisterUser = async (req: Request, res: Response): Promise<void> => {
     const { name, email, birthdate, dni, username, password } = req.body;
     try {
         const newUser: User = await postReisterServices({ name, email, birthdate, dni, username, password })
+        await enviarCorreoDeConfirmacion(email, name)
         res.status(200).json(newUser)
     } catch (error) {
         res.status(400).json({ message: error })
