2848d5e06ed797213b58ed54d7dce492ae0ad4da
LuisRGN <luisrafael1598@gmail.com>
2024-04-08 22:04:18 -0500

    se agrega la funcion para enviar un correo de cancelacion del turno

--- back/src/controllers/Turns/putTurn.ts
+++ back/src/controllers/Turns/putTurn.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from "express";
 import { putTurnSevices } from "../../services/indexServices/turnService";
+import { turnModel, userModel } from "../../config/repository";
+import { enviarCorreoDeCancelarTurno } from "../../services/nodemailer";
 
-/* ↓ Funcion del controlador que cambia el status de un turno ↓ */
+/* ↓ Funcion del controlador que cambia el status de un turno mas un correo de confirmación ↓ */
 
 export const putTurns = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
     try {
+        const turno = await turnModel.findOne({ where: { id: Number(id) }, relations: ["user"] })
+
+        const userId = turno?.user?.id;
+
+        const usuario = await userModel.findOne({ where: { id: userId }, relations: ["turns"] })
+
+        const destinatario = usuario?.email;
+        const nombreUsuario = usuario?.name;
+        const fecha = turno?.date;
+
+        if (destinatario && nombreUsuario && fecha) {
+            await enviarCorreoDeCancelarTurno(destinatario, nombreUsuario, fecha);
+        } else {
+            throw new Error("No se pudo encontrar el destinatario o el nombre del usuario");
+        }
+
         await putTurnSevices(Number(id))
         res.status(200).json({ message: "Turno cancelado" })
     } catch (error) {
