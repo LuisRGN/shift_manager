7364e3df3f86c9c9595857d543ef9fcb0f82c85f
LuisRGN <luisrafael1598@gmail.com>
2024-04-10 14:54:51 -0500

    se hace la funcion de error para redirigir al usuario a la pagina principal cuando se va a una ruta que no existe

--- front/src/views/Error/Error.tsx
+++ front/src/views/Error/Error.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+    
+    const [count, setCount] = useState(5)
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        const timeDown = setTimeout(() => {
+            setCount(count => count -1);
+            if (count === 1) navigate("/");
+        }, 1000);
+        return () => clearTimeout(timeDown)
+    }, [count, navigate])
+
+    useEffect(()=> () => {
+        setCount(5)
+    }, [])
+
     return (
         <>
-        <h1>ERROR</h1>
+        <h1>ERROR 404</h1>
+        <h2>Regresando a la pagina principal en {count}</h2>
         </>
     )
 }
