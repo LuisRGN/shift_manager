50900c50eacd0c876793cf6ee963f299043301a0
LuisRGN <luisrafael1598@gmail.com>
2024-04-10 21:18:18 -0500

    se le estilos y funcionalidad al navbar enrutando las diferentes vistas

--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,43 @@
 import React from "react";
+import styles from "./Navbar.module.css"
+import { NavLink } from "react-router-dom";
+import avatar from "../../assets/avatar.jpeg"
+import logo from "../../assets/logo.jpg"
 
 const Navbar = () => {
 return (
     <>
-    <h1>NAVBAR</h1>
+    
+    <div className={styles.container}>
+        <div>
+            <NavLink to={"/"}>
+                <img src={logo} alt="logo" className={styles.logo}/>
+            </NavLink>
+        </div>
+        
+        <div className={styles.links}>
+          <NavLink to={"/"} className={({ isActive}) => isActive ? styles.active : ""}>
+            <span>HOME</span>
+        </NavLink>
+         <NavLink to={"/Turns"} className={({ isActive}) => isActive ? styles.active : ""}>
+            <span>TURNOS</span>
+         </NavLink>
+         <NavLink to={"/Contact"} className={({ isActive}) => isActive ? styles.active : ""}>
+            <span>CONTACTO</span>
+         </NavLink>
+         <NavLink to={"/About"} className={({ isActive}) => isActive ? styles.active : ""}>
+            <span>SOBRE</span>
+         </NavLink>
+         <NavLink to={"/Login"} >
+            <span>INICIAR SESIÓN</span>
+         </NavLink>
+        <NavLink to={"/Login"}>
+        <img src={avatar} alt="avatar" className={styles.avatar} />
+        </NavLink>
+         
+        </div>
+        
+    </div>
     </>
 )
 }
