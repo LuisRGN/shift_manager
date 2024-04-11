bf1ab6da5b4c5b56c85ff00176c069d169b73a1e
LuisRGN <luisrafael1598@gmail.com>
2024-04-11 18:10:22 -0500

    se crea una slice para about

---
+++ front/src/components/Slide/SliceAbout.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from "./SliceAbout.module.css"
+import orm from "../../assets/imgT/orm.png"
+import redux from "../../assets/imgT/redux.webp"
+import typescript from "../../assets/imgT/typescript.png"
+import express from "../../assets/imgT/express.jpg"
+import postgres from "../../assets/imgT/postgres.png"
+import react from "/public/react.svg"
+import vite from "/public/vite.svg"
+
+export const SliceAbout = () => {
+  return (
+    <div className={styles.div}>
+       <div className={styles.container}>
+        <h2>Tecnologias utilizadas</h2>
+        <div className={styles.inner}>
+            <div className={styles.tag}><img src={orm} alt=""/></div>
+            <div className={styles.tag}><img src={redux} alt="" /></div>
+            <div className={styles.tag}><img src={typescript} alt="" /></div>
+            <div className={styles.tag}><img src={express} alt="" /></div>
+            <div className={styles.tag}><img src={postgres} alt="" /></div>
+            <div className={styles.tag}><img src={react} alt="" /></div>
+            <div className={styles.tag}><img src={vite} alt="" /></div>
+            <div className={styles.tag}><img src={orm} alt="" /></div>
+            <div className={styles.tag}><img src={redux} alt="" /></div>
+            <div className={styles.tag}><img src={typescript} alt="" /></div>
+            <div className={styles.tag}><img src={express} alt=""/></div>
+            <div className={styles.tag}><img src={postgres} alt="" /></div>
+            <div className={styles.tag}><img src={react} alt="" /></div>
+            <div className={styles.tag}><img src={vite} alt="" /></div>
+            
+            <div className={styles.fade}></div>
+        </div>
+    </div>
+    </div>
+  )
+}
