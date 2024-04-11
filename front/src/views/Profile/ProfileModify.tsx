fa7915885f2108a1b01586f913d01ff40602784d
LuisRGN <luisrafael1598@gmail.com>
2024-04-11 18:14:01 -0500

    se agrega el archivo para gestionar el formulario de la modificacion de usuario

---
+++ front/src/views/Profile/ProfileModify.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { FormProfileModify } from '../../components/Forms/FormProfileModify'
+
+export const ProfileModify = () => {
+  return (
+    <FormProfileModify/>
+  )
+}
