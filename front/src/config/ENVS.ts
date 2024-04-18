cc1610c152389dc09216681de56b6eb81655cf48
LuisRGN <luisrafael1598@gmail.com>
2024-04-17 23:34:21 -0500

    se crea un archivo para las variables de entorno

---
+++ front/src/config/ENVS.ts
@@ -0,0 +1,3 @@
+export const URL = import.meta.env.VITE_URL as string;
+export const HEADER_KEY = import.meta.env.VITE_RAPIDAPI_KEY as string;
+export const HEADER_HOST = import.meta.env.VITE_RAPIDAPI_HOST as string;
